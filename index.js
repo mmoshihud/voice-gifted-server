cff93364f3cb1b5229d464808d1c9fa7c241fdf5
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-06-09 14:56:42 +0600

    cors, dotenv, mongodb setup done

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.j1jxfgc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
